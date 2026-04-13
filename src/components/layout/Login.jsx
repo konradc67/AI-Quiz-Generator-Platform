c772f746b6fc42b48abaa2a70f98c3857cbb97e2
pjonaseq21 <gracjan.chamulati2019@gmail.com>
2026-04-13 16:13:12 +0200

    fix: login fix

--- src/components/layout/Login.jsx
+++ src/components/layout/Login.jsx
@@ -4,8 +4,8 @@ import 'react-toastify/dist/ReactToastify.css';
 import { Link, useNavigate } from "react-router-dom";
 
 export default function Login() {
-    const [email, setEmail] = useState('');
     const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
     const navigate = useNavigate();
     
     const [isLoading, setIsLoading] = useState(false);
@@ -65,17 +65,16 @@ export default function Login() {
 
                 <form onSubmit={handleLogin} style={{ display: 'flex', flexDirection: 'column', gap: '10px' }}>
                     <input 
-                        type="email" 
-                        placeholder="Email" 
-                        value={email}
-                        onChange={(e) => setEmail(e.target.value)}
+                        type="text" 
+                        value={username}
+                        onChange={(e) => setUsername(e.target.value)}
                         required
                     />
                     <input 
                         type="password" 
-                        placeholder="Username" 
-                        value={username}
-                        onChange={(e) => setUsername(e.target.value)}
+                        placeholder="Password" 
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
                         required
                     />
                     
